866cb6e3806e23a346f65320a79c9dd03075148b
Rugelgen <Oleg.zirkunov@gmail.com>
2024-10-20 01:04:20 +0300

    Add files via upload

---
+++ ЛР1/3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# индекс середины
+sr_index = len(list_players) // 2
+
+f_team = list_players[:sr_index]
+s_team = list_players[sr_index:]
+
+print(f_team)
+print(s_team)
