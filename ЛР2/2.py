6ada093ccd5a715ccff7cf77dbdce5962e3bb68d
Rugelgen <Oleg.zirkunov@gmail.com>
2024-10-27 02:12:18 +0300

    Add files via upload

---
+++ ЛР2/2.py
@@ -0,0 +1,16 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+zapas = 0
+increase += 1
+i = 0
+for _ in range(months):
+    money_capital = (increase**i)*spend - salary
+    zapas += money_capital
+    #print(money_capital)
+    i += 1
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(zapas,))
